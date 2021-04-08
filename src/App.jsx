888d6e3f08cbba36088502294954392d868febf5
sweeney174 <1749790593@qq.com>
2021-04-08 23:27:45 +0800

    主页ui展开，几个按钮

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+// eslint-disable-next-line
 import Home from  './components/Home/Home'
 import './App.css'
+import './assets/iconfont/iconfont.css'
 
 export default class App extends Component {
   render() {
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,12 +11,28 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <div>
-          <div className="icon">
+        {/* box--主页界面背景 */}
+        <div className="box">
+          <span className="musicMsg">来点音乐→</span>
+          <button>
+            <i className="iconfont music">&#xe60d;</i>
+          </button>
 
+          <button className="btn1"></button>
+          <button className="btn2"></button>
+          <button className="btn3"></button>
+        </div>
+
+
+        <div>
+          {/* icon--app图标 */}
+          <div className="icon">
+            <img src="" alt=""/>
+            <h2 className="appName">APP</h2>
           </div>
         </div>
-        
+
+        {/* hey--欢迎语 */}
         <div className="hey" ref={this.hey} >
           <h1>Hey,</h1>
           <h1>我是<span>APP</span></h1>
